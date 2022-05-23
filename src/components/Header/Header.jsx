44337bb2f49319029f54f5c03de20ac6a6eeb38b
snowleopardAlexa <alexaslomski@gmail.com>
2022-05-23 13:44:25 -0400

    fix sass and style logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,10 @@
-
+import './Header.scss'
 
 const Header = () => {
     return (
       <div className="header">
           <div className="header__logo">
-              <img src="/logos/logo.svg" width="100px" height="100px" alt="logo" />
+              <img src="/logos/logo.svg" width="120px" height="80px" alt="logo" />
           </div>
       </div>
     );
