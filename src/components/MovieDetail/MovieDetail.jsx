00e26c273a5e7a181c5f2977ca156929870d5514
snowleopardAlexa <alexaslomski@gmail.com>
2022-05-23 11:29:31 -0400

    add entry syntax rsc for all components

---
+++ src/components/MovieDetail/MovieDetail.jsx
@@ -0,0 +1,11 @@
+
+
+const MovieDetail = () => {
+    return (
+        <div>
+            MovieDetail
+        </div>
+    );
+};
+
+export default MovieDetail;
---
+++ src/components/MoviesList/MoviesList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MoviesList = () => {
+    return (
+        <div>
+            MovieList
+        </div>
+    );
+};
+
+export default MoviesList;
