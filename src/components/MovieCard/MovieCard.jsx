585ea5e5f0b33774c56c8fcbfd2d64435fd0515a
snowleopardAlexa <alexaslomski@gmail.com>
2022-05-23 17:14:48 -0400

    add styles to movies list, add movie card, pass data prop from movie list to movie card, destructure data, fetch poster and title of the movie in the movie card

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
-const MovieCard = () => {
+const MovieCard = (props) => {
+    // destructure data
+    const { data } = props
+
     return (
-        <div>
-            movie card
+        <div className="movie__card">
+          <img src={data.Poster} alt={data.Title} /> {/* data.Title must be fetched from API */}
         </div>
     );
 };
--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -12,6 +12,7 @@ const MoviesList = () => {
 
   showMovies = movies.Response === 'True' ? (
     movies.Search.map((movie, index) => (
+    // data - prop that we pass to MovieCard.jsx component
       <MovieCard key={index} data={movie} />
    ))
   ) : (
@@ -22,7 +23,7 @@ const MoviesList = () => {
   return (
     <div className="container">
         <div className="movie__list">
-            <div className="movie-container">{showMovies}</div>
+            <div className="movie__container">{showMovies}</div>
         </div>
       <div className="background__image"></div>
       <h3>Don't know what to search?</h3>
